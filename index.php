fdf73fc2401b2513c529657b2a8368ba6aa6612f
Revprm <revy.pramana@gmail.com>
2023-12-20 14:08:53 +0700

    fix: modify caption

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $result = $conn->query($sql);
 <html lang="en">
 
 <head>
-  <title>Tugas 5</title>
+  <title>Final Project Pweb</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
